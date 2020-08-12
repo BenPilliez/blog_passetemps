fa38064a386b3ef147d5383089e6e5482e496d9a
Pilliez Benjamin <benjamin.pilliez@etd-solutions.com>
2020-08-12 18:20:42 +0200

    ok

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -17,9 +17,9 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $user = new User();
-        $user->setUsername('demo');
+        $user->setUsername('madeleine.f');
         $user->setEmail('benjamin.pilliez@sfr.fr');
-        $user->setPassword($this->encoder->encodePassword($user, 'demo'));
+        $user->setPassword($this->encoder->encodePassword($user, 'ClemBen59'));
         $manager->persist($user);
         $manager->flush();
     }
